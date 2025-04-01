1be6fa43b30870b14753abc502560bbdc5d886d9
Andrew Deacon <andrew.deacon@equalexperts.com>
2025-04-01 09:40:54 +0100

    Refactor wdio conf files to same order

--- wdio.browserstack.conf.js
+++ wdio.browserstack.conf.js
@@ -1,50 +1,27 @@
 import fs from 'node:fs'
-import path from 'node:path'
 import { ProxyAgent, setGlobalDispatcher } from 'undici'
 import { bootstrap } from 'global-agent'
 
-/**
- * Enable webdriver.io to use the outbound proxy.
- * This is required for the test suite to be able to talk to BrowserStack.
- */
-  const dispatcher = new ProxyAgent({
-    uri: 'http://localhost:3128'
-  })
-  setGlobalDispatcher(dispatcher)
-  bootstrap()
-  global.GLOBAL_AGENT.HTTP_PROXY = 'http://localhost:3128'
+const dispatcher = new ProxyAgent({
+  uri: 'http://localhost:3128'
+})
+setGlobalDispatcher(dispatcher)
+bootstrap()
+global.GLOBAL_AGENT.HTTP_PROXY = 'http://localhost:3128'
 
 export const config = {
-  //
-  // ====================
-  // Runner Configuration
-  // ====================
-  // WebdriverIO supports running e2e tests as well as unit and component tests.
-  runner: 'local',
-  //
-  // Set a base URL in order to shorten url command calls. If your `url` parameter starts
-  // with `/`, the base url gets prepended, not including the path portion of your baseUrl.
-  // If your `url` parameter starts without a scheme or `/` (like `some/path`), the base url
-  // gets prepended directly.
-  baseUrl: `https://forms-runner-v2.${process.env.ENVIRONMENT}.cdp-int.defra.cloud`,
-
-  // You will need to provide your own BrowserStack credentials.
-  // These should be added as secrets to the test suite.
   user: process.env.BROWSERSTACK_USERNAME,
   key: process.env.BROWSERSTACK_KEY,
-
-  // Tests to run
+  baseUrl: `https://forms-runner-v2.${process.env.ENVIRONMENT}.cdp-int.defra.cloud`,
+  runner: 'local',
   specs: ['./test/specs/*.js'],
-  // Tests to exclude
   exclude: [],
   maxInstances: 1,
-
   commonCapabilities: {
     'bstack:options': {
       buildName: `ffc-grants-browser-tests-${process.env.ENVIRONMENT}`
     }
   },
-
   capabilities: [
     {
       browserName: 'Chrome',
@@ -55,18 +32,8 @@ export const config = {
         os: 'Windows',
         osVersion: '11'
       }
-    },
-    // {
-    //   browserName: 'Safari',
-    //   'bstack:options': {
-    //     idleTimeout: 300,
-    //     deviceOrientation: 'portrait',
-    //     deviceName: 'iPhone 16',
-    //     osVersion: '18'
-    //   }
-    // }
+    }
   ],
-
   services: [
     [
       'browserstack', {
@@ -82,29 +49,21 @@ export const config = {
         browserstackLocal: true,
         opts: {
           proxyHost: 'localhost',
-          proxyPort: 3128,
-          verbose: 'true'
+          proxyPort: 3128
         }
       }
     ]
   ],
-
   execArgv: ['--loader', 'esm-module-alias/loader'],
-
-  logLevel: 'debug',
-
-  // Number of failures before the test suite bails.
+  logLevel: 'info',
   bail: 0,
   waitforTimeout: 10000,
   waitforInterval: 200,
   connectionRetryTimeout: 120000,
   connectionRetryCount: 3,
-
   framework: 'mocha',
-
   reporters: [
     [
-      // Spec reporter provides rolling output to the logger so you can see it in-progress
       'spec',
       {
         addConsoleLogs: true,
@@ -113,32 +72,21 @@ export const config = {
       }
     ],
     [
-      // Allure is used to generate the final HTML report
       'allure',
       {
         outputDir: 'allure-results'
       }
     ]
   ],
-
-  // Options to be passed to Mocha.
-  // See the full list at http://mochajs.org/
   mochaOpts: {
     ui: 'bdd',
     timeout: 600000
   },
-
-  // Hooks
-  afterTest: async function (
-    test,
-    context,
-    { error, result, duration, passed, retries }
-  ) {
+  afterTest: async function (test, context, { error, result, duration, passed, retries }) {
     if (error) {
       await browser.takeScreenshot()
     }
   },
-
   onComplete: function (exitCode, config, capabilities, results) {
     // !Do Not Remove! Required for test status to show correctly in portal.
     if (results?.failed && results.failed > 0) {
