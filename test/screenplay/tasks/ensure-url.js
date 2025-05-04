118342e1421f32d687455d9a18cddb360c42391b
Andrew Deacon <andrew.deacon@equalexperts.com>
2025-05-04 11:48:53 +0100

    Refactor to native WebdriverIO expect

--- test/screenplay/tasks/ensure-url.js
+++ test/screenplay/tasks/ensure-url.js
@@ -1,5 +1,3 @@
-import { expect } from 'chai'
-
 export default class EnsureUrl {
     is(expectation) {
         this.expectation = expectation
@@ -7,25 +5,6 @@ export default class EnsureUrl {
     }
 
     async perform() {
-        var actualUrl
-        const doesActualUrlEndWithExpectedPath = await this.#pollForSuccess(async () => {
-            actualUrl = await browser.getUrl()
-            return await actualUrl.endsWith(this.expectation)
-        })
-
-        expect(doesActualUrlEndWithExpectedPath, `Expected URL to be: ${this.expectation} but was: ${actualUrl}`).to.be.true
-    }
-
-    async #pollForSuccess(predicate) {
-        const pollingLimit = 10
-        const pollingIntervalSeconds = 1
-
-        for (let i = 0; i < pollingLimit; i++) {
-            if (await predicate()) {
-                return true
-            }
-            await new Promise((resolve) => setTimeout(resolve, pollingIntervalSeconds * 1000))
-        }
-        return false
+        await expect(browser).toHaveUrl(expect.stringContaining(this.expectation))
     }
 }
